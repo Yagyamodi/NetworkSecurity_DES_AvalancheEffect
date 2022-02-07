5ff300a3cde9d5610afee6f8f71a0714f9b0fa59
Yagya Modi <44470067+Yagyamodi@users.noreply.github.com>
2022-02-07 18:20:19 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -5,10 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def alterText(txt: str, hamming_dist: int) -> str:
+def alterText(txt: str, ham_distance: int) -> str:
   """Alter the given txt at some hamming_dist number of positions. This is used to experimentally determine how the avalanche effect varies with hamming distance in DES. """
   length = len(txt)
-  random_pos = sample(range(0,length),hamming_dist)
+  random_pos = sample(range(0,length),ham_distance)
   random_pos.sort() # To ensure the for loop below doesnot increase the length of txt
   altered_txt = ""
   i = 0
@@ -35,27 +35,30 @@ key = "0110001101000000101101001010100110000000110110001110110011101011"
 # Settings
 total_rounds = 16
 midlength = 32
-hamming_dist = 1
+ham_distance = 1
 
 #output variables
-task1_diff = np.zeros((16,1))
+task2_diff = np.zeros((16,1))
 
 
 # Hamming the plaintext in binary mode
 for i in range(0,10):
-    original_pt_arr = [plaintext]
-    pt_arr = [alterText(plaintext, hamming_dist)]
+    plaintext_vector = [plaintext]
+    second_plaintext_vector = [alterText(plaintext, ham_distance)]  # plaintext array with hamming distance i+1 with plaintext_vector
     key = [key]
+    # generating round keys
     rkb,rkh = generate_round_keys(key, total_rounds, midlength)
-    original_ciphertext, original_round_ciphertexts = encrypt(original_pt_arr, rkb, total_rounds, midlength)
-    _, round_ciphertexts = encrypt(pt_arr, rkb, total_rounds, midlength)
+    # running DES algorithm for 16 rounds
+    original_ciphertext, original_round_ciphertexts = encrypt(plaintext_vector, rkb, total_rounds, midlength)
+    _, round_ciphertexts = encrypt(second_plaintext_vector, rkb, total_rounds, midlength)
+    # finding hamming distance between two ciphertexts
     ham_diff = calc_diff(original_round_ciphertexts, round_ciphertexts)
-    task1_diff = np.column_stack((task1_diff, ham_diff))
-    hamming_dist += 1
+    task2_diff = np.column_stack((task2_diff, ham_diff))
+    ham_distance += 1   # increasing hamming distance between P0 and next plaintext Pi
 
 
-task1_diff = np.delete(task1_diff, 0, 1)
-task1_diff = np.transpose(task1_diff)
+task2_diff = np.delete(task2_diff, 0, 1)
+task2_diff = np.transpose(task2_diff)
 
 fig = plt.figure(figsize =(12, 9))
 
@@ -67,7 +70,7 @@ ax.set_title("Task 2: For increasing hamming distance by 1 in plaintext, find ou
 
  
 # Creating plot
-bp = ax.boxplot(task1_diff)
+bp = ax.boxplot(task2_diff)
  
 # show plot
 plt.show()
